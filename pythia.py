819827a9a3a7ff7e52d0a28b9b57588563e18d14
E.Iosifidis <iosifidise@gmail.com>
2016-09-10 12:50:17 +0300

    Update pythia.py

--- pythia.py
+++ pythia.py
@@ -8,6 +8,10 @@ class PyPythia():
         present = True
         while present:
             mynum = int(random.random()*9007199254740992)
+            
+            while len(str(mynum)) < 21:
+                mynum = int(random.random()*900632457199254740992)
+ 
             if mynum not in self.results:
                 present = False
             self.results.append(mynum)
